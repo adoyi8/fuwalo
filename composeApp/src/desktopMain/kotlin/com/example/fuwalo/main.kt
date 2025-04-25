0875b35403e7cb63b3a0550ffa1ed455ccbc6cf8
adoyiKTL <sunday_adegbe@kuantarium.com>
2025-04-25 15:05:24 +0100

    first commit

---
+++ composeApp/src/desktopMain/kotlin/com/example/fuwalo/main.kt
@@ -0,0 +1,13 @@
+package com.example.fuwalo
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Fuwalo",
+    ) {
+        App()
+    }
+}
